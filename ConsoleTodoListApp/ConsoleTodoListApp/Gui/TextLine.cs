4b534458d0b70de97d371e9ee942112232677b7d
Raimundas Banevicius <rbanevicius@alna.lt>
2020-01-16 12:39:27 +0200

    Simple text line created.

---
+++ ConsoleTodoListApp/ConsoleTodoListApp/Gui/TextLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTodoListApp.Gui
+{
+    class TextLine
+    {
+        private int x;
+        private int y;
+        private int width;
+        private string data;
+
+        public TextLine(int x, int y, int width, string data)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.data = data;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(data.Substring(0, width));
+
+        }
+    }
+}
--- ConsoleTodoListApp/ConsoleTodoListApp/Program.cs
+++ ConsoleTodoListApp/ConsoleTodoListApp/Program.cs
@@ -13,6 +13,12 @@ namespace ConsoleTodoListApp
             Rectangle rectangle2 = new Rectangle(3, 7, 5, 7, '+');
             rectangle2.Draw();
 
+            int x = 20; 
+            int y = 2;
+            int width = 2;
+            TextLine textLine = new TextLine(x, y, width, "My super todo list app!");
+            textLine.Draw();
+
             Console.ReadKey();
 
             Console.SetCursorPosition(0, 25);
